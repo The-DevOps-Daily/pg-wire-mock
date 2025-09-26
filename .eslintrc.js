5ff0c458ea4f9ff822b903ad2f82a181ecd6d13f
Bobby Iliev <bobby@bobbyiliev.com>
2025-09-26 12:27:07 +0300

    feat: Add ESLint and Prettier configuration

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+/**
+ * ESLint configuration for pg-wire-mock
+ */
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off', // Allow console for this project as it's a CLI tool
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-trailing-spaces': 'error',
+    semi: ['error', 'always'],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'comma-dangle': ['error', 'always-multiline'],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'arrow-spacing': 'error',
+    'space-before-blocks': 'error',
+    'keyword-spacing': 'error',
+    'eol-last': ['error', 'always'],
+    'max-len': ['warn', { code: 100, ignoreComments: true }],
+  },
+  ignorePatterns: ['node_modules', 'coverage'],
+};
