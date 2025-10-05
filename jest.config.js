d32d6b36b1c1f81b80a0b5d146913492920f0d4a
Bobby Iliev <bobby@bobbyiliev.com>
2025-10-05 15:17:08 +0300

    Revert "chore: Enhance Jest configuration with coverage reporting and test setup"
    This reverts commit c66ca438eee5b00d8c6d6a2edb32ecdfbf5bb5c4.

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,8 @@
 module.exports = {
   testEnvironment: 'node',
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['src/**/*.js', '!src/index.js', '!**/node_modules/**'],
-  coverageReporters: ['text', 'lcov', 'html'],
-  testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).js'],
-  setupFilesAfterEnv: ['<rootDir>/__tests__/setup.js'],
-  testTimeout: 30000,
+  collectCoverageFrom: ['src/**/*.js', '!**/node_modules/**'],
+  coverageReporters: ['text', 'lcov'],
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
   verbose: true,
 };
