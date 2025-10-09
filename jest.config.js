605440f51bfb032e054144310f832d3de4d275b5
HUmnanANirudh <tanirudhganesh@gmail.com>
2025-10-09 15:00:20 +0530

    chore: Remove coverage thresholds and related configurations from Jest setup

--- jest.config.js
+++ jest.config.js
@@ -12,39 +12,5 @@ module.exports = {
   coverageReporters: ['text', 'lcov', 'html', 'json-summary'],
   testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
   verbose: false,
-
-  // Coverage thresholds - Set to current achievable levels
-  // These represent good test coverage while allowing for future improvement
-  coverageThreshold: {
-    global: {
-      branches: 39,
-      functions: 54,
-      lines: 53,
-      statements: 52,
-    },
-    './src/protocol/': {
-      branches: 60,
-      functions: 80,
-      lines: 75,
-      statements: 75,
-    },
-    './src/handlers/': {
-      branches: 35,
-      functions: 65,
-      lines: 50,
-      statements: 50,
-    },
-  },
-
-  // Test timeout
-  testTimeout: 10000,
-
-  // Setup files
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-
-  // Ignore patterns
-  testPathIgnorePatterns: ['/node_modules/', '/coverage/'],
-
-  // Module paths
-  modulePaths: ['<rootDir>'],
 };
